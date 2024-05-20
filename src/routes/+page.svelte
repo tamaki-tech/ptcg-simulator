af005911e82660191b072b94f9ddffc2ed5a90bf
tamaki_tech <oshaburimegane@gmail.com>
2024-05-21 01:09:48 +0900

    サイドを表示 #12

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
   import { useMachine } from "@xstate/svelte";
-  import { PtcgSimulatorMachine } from "./ptcgSimulatorMachine";
+  import { PtcgSimulatorMachine } from "./machines/index";
   import DeckCodeModal from "./components/DeckCodeModal.svelte";
   import DeckArea from "./components/DeckArea/index.svelte";
   import HandsArea from "./components/HandsArea/index.svelte";
+  import SideArea from "./components/SideArea/index.svelte";
 
   const { send, state } = useMachine(PtcgSimulatorMachine);
 
   $: deckArea = $state.context.deckArea;
   $: handArea = $state.context.handArea;
+  $: sideArea = $state.context.sideArea;
 </script>
 
 {#if $state.value !== "ready"}
@@ -16,15 +18,18 @@
     loading={$state.hasTag("loading")}
     on:click={(e) => send({ type: "searchDeck", code: e.detail })}
   />
+{:else}
+  <section class="flex justify-center items-center flex-1 h-screen">
+    <div class="grid grid-cols-4">
+      <div class="col-span-1 p-4">
+        <SideArea {sideArea} />
+      </div>
+      <div class="col-span-2 p-4">
+        <HandsArea {handArea} />
+      </div>
+      <div class="col-span-1 p-4">
+        <DeckArea {deckArea} />
+      </div>
+    </div>
+  </section>
 {/if}
-
-<section class="flex justify-center items-center flex-1 h-screen">
-  <div class="grid grid-cols-3">
-    <div class="col-span-2 p-4">
-      <HandsArea {handArea} />
-    </div>
-    <div class="col-span-1 p-4">
-      <DeckArea {deckArea} />
-    </div>
-  </div>
-</section>
---
+++ src/routes/components/SideArea/index.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import { Card } from "flowbite-svelte";
+  import type { SideAreaMachineType } from "../../machines/sideAreaMachine";
+  import PokemonCard from "../PokemonCard.svelte";
+  import { Toggle } from "flowbite-svelte";
+
+  export let sideArea: SideAreaMachineType;
+
+  let checked = true;
+
+  $: toggleLabel = checked ? "backside" : "front side";
+</script>
+
+<Card size="xs" padding="lg">
+  <h5 class="mb-4 text-lg font-medium text-gray-500 dark:text-gray-400">
+    Side ({$sideArea?.context?.cards?.length})
+  </h5>
+  <div class="grid grid-cols-2 mx-12 w-auto">
+    {#each $sideArea?.context?.cards ?? [] as card}
+      <div class="-m-4 py-4">
+        <PokemonCard
+          item={{ src: card.url, alt: card.uuid }}
+          opacity={false}
+          isReverse={checked}
+        />
+      </div>
+    {/each}
+  </div>
+  <div class="p-4">
+    <Toggle size="small" bind:checked>{toggleLabel}</Toggle>
+  </div>
+</Card>
