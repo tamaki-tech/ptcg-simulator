5c8cea6d2ce15827f1b4a29a866097b4eb34b71b
tamaki_tech <oshaburimegane@gmail.com>
2024-06-07 00:47:44 +0900

    スマートフォン画面でレイアウトが崩れないようにする #48

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <div class="flex flex-col min-h-screen">
-  <main class="flex flex-1 flex-col p-16 w-full max-w-5xl m-auto box-border">
+  <main class="flex flex-1 flex-col p-8 w-[1024px] max-w-5xl m-auto box-border">
     <slot />
   </main>
 </div>
